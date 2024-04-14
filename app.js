f02945587f1521daf192a9524a5b85b31583ce65
DaneWika2 <dane.wika@jacks.sdstate.edu>
2024-04-14 17:07:31 -0500

    Addon commit

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+});
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://danewika:10681@habitimdb.hra07pf.mongodb.net/?retryWrites=true&w=majority&appName=HabitiMDB";
